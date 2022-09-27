a08ef16ffe89f56e76b6209798d929532429352a
Seba Luna <ramon.sebastian.luna@gmail.com>
2022-09-27 17:19:17 -0300

    added style to nav bar with js

---
+++ assets/js/styleNav.js
@@ -0,0 +1,18 @@
+var links = document.querySelectorAll(".menu__item");
+
+function reset(links){
+    for (let i=0; i < links.length; i++){
+        links[i].classList.remove("activo");
+    }
+}
+
+function addEvents(links){
+    for (let i=0; i < links.length; i++){
+        links[i].addEventListener("click", function(){
+            reset(links);
+            links[i].classList.add("activo");
+        });
+    }
+}
+
+addEvents(links);
